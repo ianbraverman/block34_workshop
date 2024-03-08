b2091f6d49ec752a4e6d1f06faff329d4ff6491a
Ian <ianbraverman@hotmail.com>
2024-03-08 14:45:10 -0500

    finished seeding it and it looks good. made server.js file and now need to make the express app and route

---
+++ prisma/seed.js
@@ -0,0 +1,37 @@
+const prisma = require("../prisma/index.js");
+
+const seed = async (number = 20) => {
+  for (let i = 1; i < number; i++) {
+    await prisma.customer.upsert({
+      where: { id: i },
+      update: {},
+      create: {
+        name: `Person {i}`,
+      },
+    });
+    await prisma.restaurant.upsert({
+      where: { id: i },
+      update: {},
+      create: {
+        name: `Restaurant {i}`,
+      },
+    });
+    await prisma.reservations.upsert({
+      where: { id: i },
+      update: {},
+      create: {
+        party_count: Math.floor(Math.random() * 5),
+        customerId: i,
+        restaurantId: i,
+      },
+    });
+  }
+};
+
+seed()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
